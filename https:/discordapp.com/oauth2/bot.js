e9a15835b6cc71597b8417b7a731f44201a3bec6
codingyf <67664681+codingyf@users.noreply.github.com>
2020-06-30 16:45:04 -0700

    Create bot.js

---
+++ https:/discordapp.com/oauth2/bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready' , () => {
+    if (message.content === 'boogies plug') {
+    }
+}};
+
+// THIS MUST BE THIS WAY
+client.login(process.env.BOT_TOKEN);
